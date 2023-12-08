260ebe80a9662b13c41d4465bd37497e835a1b8d
Abhishek Patel <abhishekpatel190494@gmail.com>
2023-12-08 18:06:33 -0500

    Geometry

--- exercise/src/script.js
+++ exercise/src/script.js
@@ -13,8 +13,49 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+
+const geometry = new THREE.BufferGeometry()
+
+const count = 5000
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for (let i=0; i < count *3 *3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute("position", positionsAttribute)
+
+
+
+// const positionsArray = new Float32Array([
+//     0, 0, 0,
+//     0, 1, 0,
+//     1, 0, 0
+// ])
+
+// vertice
+// positionsArray[0] = 0
+// positionsArray[1] = 0
+// positionsArray[2] = 0
+// positionsArray[3] = 0
+// positionsArray[4] = 0
+// positionsArray[5] = 0
+// positionsArray[6] = 1
+// positionsArray[7] = 0
+// positionsArray[8] = 0
+
+
+// const positionAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+
+// geometry.setAttribute("position", positionAttribute)
+
+
+const material = new THREE.MeshBasicMaterial({ color: "darkorange", wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
